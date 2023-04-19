a31a791f1344133fc2af789938a5dc2a6032d082
joluro86 <joluro86@hotmail.com>
2023-04-18 21:55:46 -0500

    cambios codigo smell

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,30 +1,15 @@
-public class Calculadora {
+package java;
+class Calculadora {
 
-    public static double resultado;
-
-    public Calculadora() {
-        resultado = 0;
+    public double suma(double num1, int num2){
+        return num1+num2;
     }
 
-    public double suma(double num1, int num2) {
-        resultado = num1 + num2;
-        return resultado;
+    public double multiplicacion(double num1, int num2){
+        return num1*num2;
     }
 
-    public double multiplicacion(double num1, int num2) {
-        resultado = num1 * num2;
-        return resultado;
-    }
-
-    public double division(double num1, int num2) {
-        try {
-            if (num2 == 0) {
-                throw new Exception("Error: no se puede dividir por cero"); // Se lanza una excepción personalizada
-            }
-            resultado = num1 / num2;
-        } catch (Exception e) {
-            System.out.println(e.getMessage()); // Se muestra el mensaje de la excepción
-        }
-        return resultado;
+    public double division(double num1, int num2){
+        return num1/num2;
     }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculadoraTest {
+class CalculadoraTest {
 
     // Nombres de métodos y variables poco descriptivos
     Calculadora c;
